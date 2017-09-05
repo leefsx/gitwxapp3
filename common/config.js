819c79788bcc4f92f763154523359f7c0f07b50a
leefsx <leefsx@aliyun.com>
2017-09-05 11:36:11 +0800

    配置替换

--- common/config.js
+++ common/config.js
@@ -1,1 +1,103 @@
-module.exports = { "appid": "wx24b2b2feed56da32", "wxpaykey": "what147are258the369API000Partner", "apitoken": "78WnkNebQpWxbwSXYH59tW9RnKj3M4Sz", "domain": "https://lishifeng2.mywopop.com", "appsecret": "5d5c052719093405f6df5e4ae9684254", "mch_id": "1248985201", "theme": "style3", "toptitle": "企业教育网", "top400tel": "4000-000-000", "topbgcolor": "#e76565", "toptitle_color": "#fff", "logo_title": "企业教育网", "logo_subtitle": "EDUCATION", "logo_title_color": "#2c2d2c", "logo_subtitle_color": "#c3bfb8", "logo": "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/logo/logo.png", "logourl": { "url": "" }, "index_autoplay_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/swiper/bg-1.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/swiper/bg-2.jpg"], "index_autoplay_imgurl": [{ "url": "" }, { "url": "" }], "navbutton_color": "#fcb848", "navcolumn_color": "#fcb848", "navfont_color": "#fff", "homepage_aboutus_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/home_aboutus/aboutus.jpg"], "homepage_aboutus_imgurl": [{ "url": "" }], "homepage_aboutus_desc": "关于我们 ABOUT US教育培训学校是XX地区最权威的 教育培训网络服务平台之一。网站创办于2005年，以XX地区丰富的教育培训教育培训网络服务平台之一教育培训务平台之一", "homepage_aboutus_desc_color": "#fff", "homepage_aboutus_desc_bgcolor": "#fcb848", "homepage_aboutus_more": "查看详情", "homepage_aboutus_more_color": "#fcb848", "homepage_aboutus_more_bordercolor": "#fcb848", "ourservice": "我们的服务 OUR SERVICE", "ourservice_imgs": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/1.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/2.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/3.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/4.jpg"], "ourservice_imgsurl": [{ "url": "" }, { "url": "" }, { "url": "" }, { "url": "" }], "news_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/news/news.jpg"], "news_imgurl": [{ "url": "" }], "news_datasource": "", "news_bgcolor": "#eac047", "news_color": "#fff", "news_title_color": "#fff", "news_date_color": "#fff", "news_more": "查看详情", "news_more_bordercolor": "#fff", "news_more_color": "#fff", "products_title": "产品中心 PRODUCTS", "products_datasource": "", "aboutus_title": "关于我们 ABOUT US", "aboutus_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/aboutus/aboutus.jpg"], "aboutus_imgurl": [{ "url": "" }], "aboutus_desc": "教育培训学校是XX地区最权威的 教育培训网络服务平台之一。网站创办于2005年，以XX地区丰富的教育培训教育培训网络服务平台之一教育培训务平台之一", "aboutus_desc_color": "#817f79", "news_category_title": "新闻资讯", "news_category_bgcolor": "#f6f6f6", "news_category_color": "#817f79", "product_category_title": "产品中心", "product_category_bgcolor": "#fcb848", "product_category_color": "#fff", "product_category_price": "#f00", "copyright": "版权所有某某商城网", "copyright_bgcolor": "#000", "copyright_color": "#fff", "homepage_aboutus_more_url": "", "news_more_url": "" };
+module.exports = {
+    "appid": "wx24b2b2feed56da32",
+    "wxpaykey": "what147are258the369API000Partner",
+    "apitoken": "78WnkNebQpWxbwSXYH59tW9RnKj3M4Sz",
+    "domain": "https://lishifeng2.mywopop.com",
+    "appsecret": "5d5c052719093405f6df5e4ae9684254",
+    "mch_id": "1248985201",
+    "theme": "style3",
+    "toptitle": "企业教育网",
+    "top400tel": "4000-000-000",
+    "topbgcolor": "#e76565",
+    "toptitle_color": "#fff",
+    "logo_title": "企业教育网",
+    "logo_subtitle": "EDUCATION",
+    "logo_title_color": "#2c2d2c",
+    "logo_subtitle_color": "#c3bfb8",
+    "logo": "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/logo/logo.png",
+    "logourl": {
+        "url": ""
+    },
+    "index_autoplay_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/swiper/bg-1.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/swiper/bg-2.jpg"],
+    "index_autoplay_imgurl": [{
+        "url": ""
+    },
+    {
+        "url": ""
+    }],
+    "navbutton_color": "#fcb848",
+    "navcolumn_color": "#fcb848",
+    "navfont_color": "#fff",
+    "homepage_aboutus_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/home_aboutus/aboutus.jpg"],
+    "homepage_aboutus_imgurl": [{
+        "url": ""
+    }],
+    "homepage_aboutus_desc": "关于我们 ABOUT US\n教育培训学校是XX地区最权威的 教育培训网络服务平台之一。网站创办于2005年，以XX地区丰富的教育培训教育培训网络服务平台之一教育培训务平台之一",
+    "homepage_aboutus_desc_color": "#fcb848",
+    "homepage_aboutus_desc_bgcolor": "#fcb848",
+    "homepage_aboutus_more": "查看详情",
+    "homepage_aboutus_more_color": "#fcb848",
+    "homepage_aboutus_more_bordercolor": "#fcb848",
+    "ourservice": "我们的服务 OUR SERVICE",
+    "ourservice_imgs": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/1.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/2.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/3.jpg", "http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/services/4.jpg"],
+    "ourservice_imgsurl": [{
+        "url": ""
+    },
+    {
+        "url": ""
+    },
+    {
+        "url": ""
+    },
+    {
+        "url": ""
+    }],
+    "news_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/news/news.jpg", ""],
+    "news_imgurl": [{
+        "url": ""
+    },
+    {
+        "url": ""
+    },
+    {
+        "url": ""
+    },
+    {
+        "url": ""
+    },
+    {
+        "url": ""
+    },
+    {
+        "url": ""
+    }],
+    "news_datasource": "23,22,21,20",
+    "news_bgcolor": "#eac047",
+    "news_color": "#fff",
+    "news_title_color": "#fff",
+    "news_date_color": "#fff",
+    "news_more": "查看详情",
+    "news_more_bordercolor": "#fff",
+    "news_more_color": "#fff",
+    "products_title": "产品中心 PRODUCTS",
+    "products_datasource": "",
+    "aboutus_title": "关于我们 ABOUT US",
+    "aboutus_img": ["http://lishifeng2.mywopop.com/template/default/images/wxapplet/style3/aboutus/aboutus.jpg"],
+    "aboutus_imgurl": [{
+        "url": ""
+    }],
+    "aboutus_desc": "教育培训学校是XX地区最权威的 教育培训网络服务平台之一。网站创办于2005年，以XX地区丰富的教育培训教育培训网络服务平台之一教育培训务平台之一",
+    "aboutus_desc_color": "#817f79",
+    "news_category_title": "新闻资讯",
+    "news_category_bgcolor": "#f6f6f6",
+    "news_category_color": "#817f79",
+    "product_category_title": "产品中心",
+    "product_category_bgcolor": "#fcb848",
+    "product_category_color": "#fff",
+    "product_category_price": "#f00",
+    "copyright": "版权所有某某商城网",
+    "copyright_bgcolor": "#000",
+    "copyright_color": "#fff",
+    "homepage_aboutus_more_url": "",
+    "news_more_url": ""
+};
